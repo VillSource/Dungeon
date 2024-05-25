240fcd1171772de4ca005d95d425caa974953cdc
villsource <anirut1800@gmail.com>
2024-05-26 04:39:17 +0700

    add simple render

--- projects/example/src/app/app.component.ts
+++ projects/example/src/app/app.component.ts
@@ -1,5 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 import { DungeonComponent, DungeonGithubService } from "dungeon";
 
 @Component({
@@ -7,28 +8,35 @@ import { DungeonComponent, DungeonGithubService } from "dungeon";
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, DungeonComponent]
+  imports: [RouterOutlet, DungeonComponent, CommonModule]
 })
 export class AppComponent implements OnInit {
   private owner = "villsource";
   private repo = "Dungeon";
-  private branch = "dev";
+  private branch = "contents";
 
   title = 'example';
   tree: string[] = []
   content: string = ""
+  rest: any
 
-  constructor(private ghDungeon: DungeonGithubService) { }
+  constructor(
+    private ghDungeon: DungeonGithubService,
+    private router: Router
+  ) { }
 
   async ngOnInit() {
     const res = await this.ghDungeon.getRepoTree(this.owner, this.repo, this.branch)
-    const tmp = res.map(i => i.path ?? "");
+    const tmp = res.map(i => i.path ?? "").filter(i=>! /.info.json$/.test(i));
     this.tree = tmp
+    this.rest = JSON.stringify(res, null, '  ');
   }
 
-  async onSelected(file:string){
-    const res = await this.ghDungeon.getRepoContent(this.owner, this.repo,this.branch, file);
-    this.content = res;
+  async onSelected(file: string) {
+    const res = await this.ghDungeon.getRepoContent(this.owner, this.repo, this.branch, file);
+    // this.content = res;
+    this.rest = res;
+    this.router.navigate(["content", ...file.replaceAll(".json","").split('/').slice(1)])
   }
 
 }
