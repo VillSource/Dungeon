e8ddf49b84f0a7639a6ee892206268b541406641
villsource <anirut1800@gmail.com>
2024-05-26 03:00:56 +0700

    add read data from gh

--- projects/dungeon/src/lib/dungeon.service.ts
+++ projects/dungeon/src/lib/dungeon.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Octokit } from 'octokit';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,34 @@ import { Injectable } from '@angular/core';
 export class DungeonService {
 
   constructor() { }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DungeonGithubService {
+  private readonly octokit: Octokit;
+
+  constructor(octokit:Octokit) {
+    this.octokit = octokit;
+  }
+
+  async getRepoTree(owner: string, repo: string, branch: string) {
+    const a = await this.octokit.rest.git.getTree({
+      owner,
+      repo,
+      tree_sha: branch,
+      recursive: "true"
+    });
+
+    const rest = a?.data?.tree?.filter(i => i.type == "blob");
+    return rest;
+    // return JSON.stringify(rest, null, "\t");
+  }
+
+  async getRepoContent(owner: string, repo: string, branch: string, path: string) {
+    const a = await fetch(`https://raw.githubusercontent.com/${owner}/${repo}/${branch}/${path}`)
+    const b = await a.text();
+    return b;
+  }
 }
